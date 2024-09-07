9ef0464534eaed5ce841fbb23d13df7c45de850b
CarlosH505 <carlinhoshenrique505@gmail.com>
2024-09-06 21:46:12 -0300

    build

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : 'God of War',
+        descricao : 'God of War é uma franquia de videogames de ação e aventura que conquistou milhões de fãs ao redor do mundo com suas narrativas épicas, combates frenéticos e visuais impressionantes. A série acompanha a jornada de Kratos, um espartano que, após trair os deuses do Olimpo, busca redenção em um mundo nórdico, onde enfrenta novos desafios e forja um vínculo com seu filho, Atreus.',
+        link : 'https://www.playstation.com/pt-br/god-of-war/',
+        tags : 'deus da guerra cleiton'
+    
+    },
+    {
+        titulo : 'Grand Theft Auto V',
+        descricao : 'Grand Theft Auto V é um dos jogos mais populares de todos os tempos, oferecendo uma experiência de mundo aberto imersiva em Los Santos. Os jogadores assumem o controle de três protagonistas distintos e vivenciam uma história épica de crime, traição e ambição.',
+        link : 'https://www.rockstargames.com/GTA5/',
+        tags : 'tiro arma matar'
+    },
+    {
+        titulo : 'Detroit: Become Human',
+        descricao : 'Detroit: Become Human é um jogo de aventura interativo que explora temas como inteligência artificial, liberdade e o que significa ser humano. Os jogadores controlam androides que estão despertando para a consciência e lutam por seus direitos em um futuro distópico.',
+        link : 'https://www.quanticdream.com/games/detroit',
+        tags : 'ia inteligencia artificial androdid robo'
+    }
+    
+]
+
